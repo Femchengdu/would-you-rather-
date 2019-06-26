123409a516f1a2301f410d5cf1ee28939d7efef3
Femi Ajayi <femiajayi705@gmail.com>
2019-06-26 21:28:35 +0800

    Changed User to a functional component

--- src/components/User.js
+++ src/components/User.js
@@ -1,28 +1,26 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 import {formatUser} from '../utils/helper'
 import Avatar from './Avatar'
 import './css/user.css'
 
 
-class User extends Component {
-	
-	render() {
-		const {name, createdQuestions, answeredQuestions, score, avatar} = this.props
-		return(
-			<article className='userContainer'>
-				<div className='flexItem'>
-					<Avatar authorsAvatar={avatar} size={120} authorsName={name}/>
-				</div>
-				<section className='flexItem'>
-					<h3>{name}</h3>
-					<p>Answered questions {answeredQuestions}</p>
-					<p>Created questions  {createdQuestions}</p>
-					<p>Score  {score}</p>
-				</section>	
-			</article>
-		)
-	}
+
+const User = (props) => {
+	const {name, createdQuestions, answeredQuestions, score, avatar} = props
+	return (
+		<article className='userContainer'>
+			<div className='flexItem'>
+				<Avatar authorsAvatar={avatar} size={120} authorsName={name}/>
+			</div>
+			<section className='flexItem'>
+				<h3>{name}</h3>
+				<p>Answered questions {answeredQuestions}</p>
+				<p>Created questions  {createdQuestions}</p>
+				<p>Score  {score}</p>
+			</section>	
+		</article>
+	)
 }
 
 
